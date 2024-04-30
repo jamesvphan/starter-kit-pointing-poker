8dfd3b55f6fb70c2c58f7d6872d921f6a1011a6f
reinaldosimoes <rsi@hellofresh.com>
2024-04-30 11:40:21 -0400

    ant design

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 
+// https://ant.design/components/overview
+import { Layout, Typography, Button } from 'antd';
+
+const { Header, Content, Footer } = Layout;
+
 const socket = io('http://localhost:3001'); // Connect to the server
 
 function App() {
@@ -19,21 +24,29 @@ function App() {
   };
 
   return (
-    <div>
-      <h1>Sprint Pointing Poker</h1>
-      <div>
-        <button onClick={() => handleVote('1')}>1 Point</button>
-        <button onClick={() => handleVote('2')}>2 Points</button>
-        <button onClick={() => handleVote('3')}>3 Points</button>
-        {/* Add more buttons as needed */}
-      </div>
-      <h2>Votes:</h2>
-      <ul>
-        {votes.map((vote, index) => (
-          <li key={index}>{vote}</li>
-        ))}
-      </ul>
-    </div>
+    <Layout>
+      <Header style={{ display: 'flex', alignItems: 'center', backgroundColor: 'white' }}>
+        <Typography.Title>Pointing Poker</Typography.Title>
+      </Header>
+
+      <Content style={{ padding: '20px 48px', display: 'flex', flexDirection: 'column' }}>
+        <div>
+          <Button onClick={() => handleVote('1')}>1 Point</Button>
+          <Button onClick={() => handleVote('2')}>2 Points</Button>
+          <Button onClick={() => handleVote('3')}>3 Points</Button>
+          {/* Add more buttons as needed */}
+        </div>
+     
+        <div style={{ padding: '20px 0'}}>
+          <Typography.Text>Votes:</Typography.Text>
+          <ul>
+            {votes.map((vote, index) => (
+              <li key={index}>{vote}</li>
+            ))}
+          </ul>
+        </div>
+      </Content>
+    </Layout>
   );
 }
 
